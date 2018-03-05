edca652b4dc52d04d434d79bd0efa0c0cbf2fec9
Sam Mckay <sammckay12@gmail.com>
2018-03-05 11:41:49 +0000

    categories displaying capitalised

--- src/components/placeHeader.js
+++ src/components/placeHeader.js
@@ -1,12 +1,25 @@
 import React, { Component } from 'react';
-import './placeHeader.css';
 
 export default class PlaceHeader extends Component {
 
   constructor (props) {
      super(props)
-     this.state = {}
+     this.state = {
+       categories: ""
+     }
    }
+
+   componentDidMount () {
+     this.createCategory()
+   }
+
+   createCategory () {
+     let string = this.props.mainPlace.category.join(", ")
+     let capitalCategories = string.replace(/\w\S*/g, function(txt){return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();});
+     this.setState({categories:  capitalCategories})
+   }
+
+
 
    render () {
      console.log("place", this.props);
@@ -14,13 +27,9 @@ export default class PlaceHeader extends Component {
 
        <div style={styles.container}>
          <p style={styles.textContainer}>{this.props.mainPlace.name}</p>
-           <p style={styles.textContainer}>{this.props.mainPlace.city}</p>
-           <li className="categoryList">
-           {this.props.mainPlace.category.map(title =>
-               <ul className="categoryList" key={title}>{title}</ul>
-             )}
+         <p style={styles.textContainer}>{this.props.mainPlace.city}</p>
+         <p style={styles.textContainer}>{this.state.categories}</p>
 
-           </li>
        </div>
      )
    }
@@ -30,7 +39,7 @@ let styles = {
   container: {
     display: 'flex',
     height: 250 ,
-    backgroundColor: '#F1Fzx1F1',
+    backgroundColor: '#F1F1F1',
     border: 'solid',
     borderWidth: 1,
     justifyContent: 'center',
